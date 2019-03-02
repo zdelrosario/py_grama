6b87cc145a770fec663c577bacbd028c9c46de1d
zach <zdelrosario@outlook.com>
2019-03-02 10:09:25 -0800

    simple example of import and usage

---
+++ examples/simple_pipeline.py
@@ -0,0 +1,16 @@
+## Simple model pipeline example
+import grama.core as gr
+import pandas as pd
+
+from grama.core import pi # Import pipe
+
+model_default = gr.model_()
+df_default    = pd.DataFrame(
+    data = {"x" : [0, 1]}
+)
+
+df_res = \
+    model_default |pi| \
+    gr.eval_df(
+        df = df_default
+    )
