9ef4768a2ac4035e3d75c4d4d89e4e9b55697da4
zdelrosario <zdelrosario@outlook.com>
2019-11-28 08:05:50 -0800

    sweep code; MCS uses random not lhs

--- grama/evals/random_sampling.py
+++ grama/evals/random_sampling.py
@@ -31,7 +31,7 @@ def ev_monte_carlo(model, n_samples = 1, seed = None, append = True):
         np.random.seed(seed)
 
     ## Draw samples
-    quantiles = lhs(model.n_in, samples = n_samples)
+    quantiles = np.random.random((n_samples, model.n_in))
 
     ## Convert samples to desired marginals
     samples = model.sample_quantile(quantiles)
@@ -86,3 +86,34 @@ def ev_lhs(model, n_samples = 1, seed = None, append = True, criterion = None):
     )
 
     return core.ev_df(model, df = df_inputs, append = append)
+
+## Marginal sweeps with random origins
+@curry
+def ev_sweeps_marginal(model, n_density=10, n_sweeps=3, append=True):
+    """Perform sweeps over each model marginal
+    """
+    ## Build quantile sweep data
+    q_random = np.random.random((n_density, model.n_in, n_sweeps))
+    q_dense  = np.linspace(0, 1, num=n_density)
+    Q_all    = np.zeros((n_density * n_sweeps * model.n_in, model.n_in))
+    C_labels = ["tmp"] * (n_density * n_sweeps * model.n_in)
+
+    ## Interlace
+    for i_input in range(model.n_in):
+        ind_base = i_input * n_density * n_sweeps
+        for i_sweep in range(n_sweeps):
+            ind_start = ind_base + i_sweep*n_density
+            ind_end   = ind_base + (i_sweep+1)*n_density
+
+            Q_all[ind_start:ind_end]          = q_random[:, :, i_sweep]
+            Q_all[ind_start:ind_end, i_input] = q_dense
+            C_labels[ind_start:ind_end] = \
+                [model.domain.inputs[i_input] + "_s" + str(i_sweep)] * \
+                n_density
+
+    ## Apply
+    df_inputs = pd.DataFrame(data=Q_all, columns=model.domain.inputs)
+    df_result = core.ev_df(model, df=df_inputs, append=append)
+    df_result["sweep_var"] = C_labels
+
+    return df_result
