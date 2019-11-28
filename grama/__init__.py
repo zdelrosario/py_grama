66cd4838cb3a89c92ed5b4161b52ca056cf8962a
zach <zdelrosario@outlook.com>
2019-11-28 12:09:36 -0800

    rename

--- grama/__init__.py
+++ grama/__init__.py
@@ -1,5 +1,5 @@
 from .core import pi, ev_df
-from .evals import ev_monte_carlo, ev_lhs, ev_sweeps_marginal
+from .evals import ev_monte_carlo, ev_lhs, ev_sinews
 from .evals import ev_nominal, ev_conservative, ev_grad_fd
 from .fitting import ft_ols
 from .compositions import cp_metamodel
