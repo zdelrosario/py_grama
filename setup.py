87607fc87b580af39a0aabdb2cc14899519739a4
zdelrosario <zdelrosario@outlook.com>
2019-12-24 20:03:47 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="py_grama",
+    version="0.1dev",
+    packages=["grama", ],
+    license="MIT",
+    long_description=open('README.md').read()
+)
