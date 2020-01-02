804c1c5e5af9f88fe529bf9539ab3682fa77db9a
zdelrosario <zdelrosario@outlook.com>
2020-01-02 09:34:34 -0800

    tf tool tests

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -7,7 +7,7 @@ import sys
 from context import grama as gr
 from context import data
 
-## Test the built-in datasets
+## Test transform tools
 ##################################################
 class TestTools(unittest.TestCase):
 
@@ -34,24 +34,24 @@ class TestTools(unittest.TestCase):
         df_res = gr.tran_bootstrap(
             df_stang,
             tran=tran_stats,
-            n_boot=3.1,
-            n_sub=3.1,
+            n_boot=3e0,
+            n_sub=3e0,
             seed=101
         )
 
         df_sel = gr.tran_bootstrap(
             df_stang,
             tran=tran_stats,
-            n_boot=3.1,
-            n_sub=3.1,
+            n_boot=3e0,
+            n_sub=3e0,
             seed=101,
             col_sel=["mean"]
         )
 
         df_piped = df_stang >> gr.tf_bootstrap(
             tran=tran_stats,
-            n_boot=3.1,
-            n_sub=3.1,
+            n_boot=3e0,
+            n_sub=3e0,
             seed=101
         )
 
@@ -72,3 +72,90 @@ class TestTools(unittest.TestCase):
 
         ## Test pipe
         self.assertTrue(gr.df_equal(df_res, df_piped))
+
+    def test_outer(self):
+        df = pd.DataFrame(dict(x=[1,2]))
+        df_outer = pd.DataFrame(dict(y=[3,4]))
+
+        df_true = pd.DataFrame(dict(
+            x=[1,2,1,2],
+            y=[3,3,4,4]
+        ))
+
+        df_res = gr.tran_outer(df, df_outer)
+        df_piped = df >> gr.tf_outer(df_outer)
+
+        pd.testing.assert_frame_equal(
+            df_true,
+            df_res,
+            check_exact=False,
+            check_dtype=False,
+            check_column_type=False
+        )
+        pd.testing.assert_frame_equal(
+            df_piped,
+            df_res,
+            check_exact=False,
+            check_dtype=False,
+            check_column_type=False
+        )
+
+# --------------------------------------------------
+class TestReshape(unittest.TestCase):
+    def setUp(self):
+        ## Transpose test
+        self.df_rownames = pd.DataFrame({
+            "rowname": ["a", "b"],
+            "x":       [0, 1],
+            "y":       [2, 3],
+        })
+        self.df_transposed = pd.DataFrame({
+            "rowname": ["x", "y"],
+            "a":       [ 0, 2],
+            "b":       [ 1, 3]
+        })
+
+        ## Gather test
+        self.df_wide = pd.DataFrame({
+            "a": [0],
+            "b": [1]
+        })
+        self.df_gathered = pd.DataFrame({
+            "key": ["a", "b"],
+            "value": [0, 1]
+        })
+
+        ## Spread test
+        self.df_long = pd.DataFrame({
+            "key":   ["a", "b"],
+            "value": [  0,   1]
+        })
+        self.df_spreaded = pd.DataFrame({
+            "index": ["value"], "a": [0], "b": [1]
+        })
+        self.df_spreaded_drop = pd.DataFrame({
+            "a": [0], "b": [1]
+        })
+
+    def test_gather(self):
+        df_res = gr.tran_gather(self.df_wide, "key", "value", ["a", "b"])
+        self.assertTrue(self.df_gathered.equals(df_res))
+
+        ## Test pipe
+        df_piped = self.df_wide >> gr.tf_gather("key", "value", ["a", "b"])
+        self.assertTrue(df_res.equals(df_piped))
+
+    def test_spread(self):
+        df_res = gr.tran_spread(self.df_long, "key", "value")
+        self.assertTrue(self.df_spreaded.equals(df_res))
+
+        ## Test pipe
+        df_piped = self.df_long >> gr.tf_spread("key", "value")
+        self.assertTrue(df_res.equals(df_piped))
+
+        ## Test with drop
+        self.assertTrue(
+            self.df_spreaded_drop.equals(
+                gr.tran_spread(self.df_long, "key", "value", drop=True)
+            )
+        )
