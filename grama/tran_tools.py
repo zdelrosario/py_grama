23929c37157942e7293ae2723bf8af5ad2d9b4e8
zdelrosario <zdelrosario@outlook.com>
2020-01-14 20:33:19 -0800

    fix iloc issue

--- grama/tran_tools.py
+++ grama/tran_tools.py
@@ -174,12 +174,13 @@ def tran_outer(df, df_outer):
         >>> 1  2  3
         >>> 2  1  4
         >>> 3  2  4
+
     """
     n_rows = df.shape[0]
     list_df = []
 
     for ind in range(df_outer.shape[0]):
-        df_rep = pd.concat([df_outer.loc[[ind]]] * n_rows, ignore_index=True)
+        df_rep = pd.concat([df_outer.iloc[[ind]]] * n_rows, ignore_index=True)
         list_df.append(pd.concat((df, df_rep), axis=1))
 
     return pd.concat(list_df, ignore_index=True)
