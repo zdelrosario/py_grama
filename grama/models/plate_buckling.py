b11a22428528709b70f4f559dc6df44a0b84a538
zdelrosario <zdelrosario@outlook.com>
2020-01-22 10:04:29 -0500

    added width; edit defaults

--- grama/models/plate_buckling.py
+++ grama/models/plate_buckling.py
@@ -7,24 +7,25 @@ from grama.data import df_stang
 LOAD      = 0.00128 # Applied load (kips)
 
 ## Reference geometry
-THICKNESS = 0.25 # Plate thickness (in)
-HEIGHT    = 12. # Plate height    (in)
+THICKNESS = 0.06 # Plate thickness (in)
+HEIGHT    = 12.  # Plate height    (in)
 
 def function_buckle_state(x):
-    t, h, E, mu, L = x
-    return np.pi * E / 12 / (1 - mu**2) * (t / h)**2 - L
+    t, h, w, E, mu, L = x
+    return np.pi * E / 12 / (1 - mu**2) * (t / h)**2 - L / t / w
 
 def make_plate_buckle():
     md = gr.Model("Plate Buckling") >> \
          gr.cp_function(
              fun=function_buckle_state,
-             var=["t", "h", "E", "mu", "L"],
+             var=["t", "h", "w", "E", "mu", "L"],
              out=["g_buckle"],
              name="limit state"
          ) >> \
          gr.cp_bounds(
-             t=(0, 2 * THICKNESS),
+             t=(0.5 * THICKNESS, 2 * THICKNESS),
              h=(6, 18),
+             w=(6, 18),
              L=(LOAD / 2, LOAD * 2)
          ) >> \
          gr.cp_marginals(
