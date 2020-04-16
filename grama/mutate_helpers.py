5f0a1a973dfa4f3edcb01e2bd12f73ec4b0b2f05
Zach <zdelrosario@outlook.com>
2020-04-15 19:47:34 -0700

    developing factor helpers

--- grama/mutate_helpers.py
+++ grama/mutate_helpers.py
@@ -8,21 +8,26 @@ __all__ = [
     "as_int",
     "as_float",
     "as_str",
+    "as_factor",
+    "fct_reorder",
 ]
 
 from grama import make_symbolic
 
+from numpy import argsort, array, median, zeros
 from numpy import sin as npsin
 from numpy import cos as npcos
 from numpy import log as nplog
 from numpy import exp as npexp
 from numpy import sqrt as npsqrt
 from numpy import power as nppower
+from pandas import Categorical
 
 # --------------------------------------------------
 # Mutation helpers
 # --------------------------------------------------
 # Numeric
+# -------------------------
 @make_symbolic
 def sin(x):
     return npsin(x)
@@ -54,6 +59,7 @@ def pow(x, p):
 
 
 # Casting
+# -------------------------
 @make_symbolic
 def as_int(x):
     return x.astype(int)
@@ -67,3 +73,23 @@ def as_float(x):
 @make_symbolic
 def as_str(x):
     return x.astype(str)
+
+
+@make_symbolic
+def as_factor(x, categories=None, ordered=True, dtype=None):
+    return Categorical(x, categories=categories, ordered=ordered, dtype=dtype)
+
+
+# Factors
+# -------------------------
+@make_symbolic
+def fct_reorder(f, x, fun=median):
+    # Get factor levels
+    levels = array(list(set(f)))
+    # Compute given fun over associated values
+    values = zeros(len(levels))
+    for i in range(len(levels)):
+        mask = f == levels[i]
+        values[i] = fun(x[mask])
+    # Sort according to computed values
+    return as_factor(f, categories=levels[argsort(values)], ordered=True)
