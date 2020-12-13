c932f6aca04596c90344b63260a59375edf2411c
Zach <zdelrosario@outlook.com>
2020-12-13 15:06:21 -0500

    version bump to 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="py_grama",
     author="Zachary del Rosario",
     author_email="zdelrosario@outlook.com",
-    version="0.1.5",
+    version="0.1.6",
     packages=[
         "grama",
         "grama.data",
