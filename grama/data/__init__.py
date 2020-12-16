87a14317fd2037d49a53ba2527f9bcedaed2a987
Zach <zdelrosario@outlook.com>
2020-12-16 15:51:27 -0500

    add ref for Ruff (1984)

--- grama/data/__init__.py
+++ grama/data/__init__.py
@@ -35,6 +35,15 @@ Datasets:
 
         ang (degrees): Angle of test to alloy roll direction
 
+    df_ruff: Metal data from Ruff (1984). Columns:
+        part: Part identifier
+
+        TYS: Tensile Yield Stress (ksi)
+
+        TUS: Tensile Ultimate Stress (ksi)
+
+        thickness: Part thickness (in)
+
     df_trajectory_full: Simulated trajectory data. Columns:
 
         t: Time since projectile launch (seconds)
@@ -46,6 +55,9 @@ Datasets:
 References:
     Stang, Greenspan, and Newman, "Poisson's ratio of some structural alloys for
     large strains" (1946) U.S. Department of Commerce National Bureau of Standards
+
+    Ruff, Paul E. An Overview of the MIL-HDBK-5 Program. BATTELLE COLUMBUS DIV
+    OH, 1984.
 """
 
 from .datasets import *
