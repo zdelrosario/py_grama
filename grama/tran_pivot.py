ca487eb049e9affdc0b805c241932525e14961ea
Oscar De La Garza <odelagarza@olin.edu>
2021-07-20 15:20:31 -0400

    pivot_longer and pivot_wider init builds complete
    pivot_longer and pivot_wider both have working tests and functionality
    now, as well as piped versions. Fixed issues when indexes_to or
    indexes_from were equal to None

--- grama/tran_pivot.py
+++ grama/tran_pivot.py
@@ -6,16 +6,9 @@ __all__ = [
 ]
 
 from grama import add_pipe
-from pandas import DataFrame, MultiIndex, Series, pivot
+from pandas import DataFrame, IndexSlice, MultiIndex, RangeIndex, Series, \
+    pivot, pivot_table
 
-
-    # TODO:
-    # -Fix Documentation on functions/make them more clear
-    # -double check on bigger datasets if they both work
-    # -naming conventions?
-    # -look into what other tests pandas uses and which we want
-    # -add notifiers to the tests
-    # -add tests for piped versions
 
 def tran_pivot_longer (
     df,
@@ -39,14 +32,60 @@ def tran_pivot_longer (
     number of columns.
 
     Args:
-        df (pandas.DataFrame): DataFrame passed through
-        columns (str, tuple, list, ndarray): Label of column(s) to pivot into longer format
-        indexes_to(str): str to specify the name of the index column. If name is already present index_to = None
-        names_to (str): name
-        values_to (str):
+        df (DataFrame): DataFrame passed through
+        columns (str): Label of column(s) to pivot into longer format
+        indexes_to(str): str to specify the name(s) of the index column,
+                         if name is already present index_to = None
+        names_to (str): name to use for the 'variable' column, if None frame.columns.name
+                        is used or ‘variable’
+        values_to (str): name to use for the 'value' column
+
+    Returns:
+        DataFrame: result of being pivoted into a longer format
+
+    Examples:
+
+        >>> import grama as gr
+        >>> from pandas import DataFrame
+        >>> wide = DataFrame(
+                {
+                    "One": {"A": 1.0, "B": 2.0, "C": 3.0},
+                    "Two": {"A": 1.0, "B": 2.0, "C": 3.0},
+                }
+            )
+        >>> long = gr.tran_pivot_longer(
+                    wide,
+                    columns=("One","Two"),
+                    index_to="index",
+                    names_to="columns",
+                    values_to="values")
+
     """
 
-    return df.reset_index().melt(id_vars=index_to, var_name=names_to, value_vars=columns, value_name=values_to)
+    if index_to is None:
+        df_columns = df.columns
+        df_used = columns
+        df_index = [x for x in df_columns if x not in df_used]
+        if not df_index:
+            return df.reset_index().melt(
+                id_vars="index",
+                var_name=names_to,
+                value_vars=columns,
+                value_name=values_to
+                )
+        return df.reset_index().melt(
+            id_vars=df_index,
+            var_name=names_to,
+            value_vars=columns,
+            value_name=values_to
+            )
+
+    return df.reset_index().melt(
+        id_vars=index_to,
+        var_name=names_to,
+        value_vars=columns,
+        value_name=values_to
+        )
 
 tf_pivot_longer = add_pipe(tran_pivot_longer)
 
@@ -54,32 +93,123 @@ tf_pivot_longer = add_pipe(tran_pivot_longer)
 def tran_pivot_wider (
      df,
      #id_cols,
-     indexes,
      names_from,
+     indexes_from = None,
      #names_prefix,
      #names_sep,
      #names_glue = None,
      #names_sort = False,
      #names_glue,
-     values_from,
+     values_from = None,
      #values_fill = None,
      #values_fn = None
  ):
-     """
+    """
 
      "Widens" data by increasing the number of columns and decreasing the
      number of rows.
 
      Args:
-         df (pandas.DataFrame): DataFrame passed through
-         names_from (str):
-         values_from (str):
-     """
+         df (DataFrame): DataFrame passed through
+         names_from (str): Column(s) name(s) to use to make new columns
+         indexes_from (str): Column(s) to use to make new index, if None will
+                             preserve and use unspecified columns as index
+         values_from (str): Column(s) to use as new values column(s)
 
-     wider = pivot(df,index=indexes,columns=names_from,values=values_from)
-     wider.index.name = None
-     wider.columns.name = None
+     Returns:
+        DataFrame: result of being pivoted wider
 
-     return wider
+     Example:
+        >>> import grama as gr
+        >>> from pandas import DataFrame
+        >>> long = DataFrame(
+                {
+                    "index": ["A", "B", "C", "A", "B", "C"],
+                    "columns": ["One", "One", "One", "Two", "Two", "Two"],
+                    "values": [1.0, 2.0, 3.0, 1.0, 2.0, 3.0],
+                }
+            )
+        >>> wide = gr.tran_pivot_wider(
+                long,
+                indexes_from="index",
+                names_from="columns",
+                values_from="values"
+            )
+    """
+
+    if indexes_from is None:
+        ### Clean columns list to find unused columns and preserve them
+        df_columns = df.columns
+        df_values_used = values_from
+        df_columns_used = names_from
+        df_1st_clean = [x for x in df_columns if x not in df_columns_used]
+        df_index_clean = [x for x in df_1st_clean if x not in df_values_used]
+
+        ### try pivot, if no repeats exist in df_index_clean values continue,
+        ### else use pivot_table to handle repreated values
+        try:
+            wider = pivot(
+                df,
+                index=df_index_clean,
+                columns=names_from,
+                values=values_from
+            )
+        except ValueError:
+            wider = pivot_table(
+                df,
+                index=df_index_clean,
+                columns=names_from,
+                values=values_from
+            )
+
+        ### Post cleaning of leftover column and index names
+        wider.columns.name = None
+        # preserve rows by re-inserting them
+        for i in range(len(df_index_clean)):
+            wider.insert(
+                loc=i,
+                column=df_index_clean[i],
+                value=wider.index.get_level_values(df_index_clean[i])
+                )
+        # remake index to be numbered column with no name
+        wider.index = RangeIndex(start=0,stop=len(wider),step=1)
+        wider.index.name = None
+
+        return wider
+    ### If indexes_from exists do the same repeat check as above but with the
+    ### provided indexes
+    try:
+        wider = pivot(
+            df,
+            index=indexes_from,
+            columns=names_from,
+            values=values_from
+        )
+    except ValueError:
+        wider = pivot_table(
+            df,
+            index=indexes_from,
+            columns=names_from,
+            values=values_from
+        )
+
+    ### Post cleaning of leftover column and index names
+    wider.columns.name = None
+    ## preserve rows by re-inserting them
+    if isinstance(indexes_from, str):
+        wider.insert(0,indexes_from,wider.index)
+    # if str insert as so, else loop through list type of indexes_from
+    else:
+        for i in range(len(indexes_from)):
+            wider.insert(
+                loc=i,
+                column=indexes_from[i],
+                value=wider.index.get_level_values(indexes_from[i])
+                )
+    # remake index to be numbered column with no name
+    wider.index = RangeIndex(start=0,stop=len(wider),step=1)
+    wider.index.name = None
+
+    return wider
 
 tf_pivot_wider = add_pipe(tran_pivot_wider)
--- tests/test_pivot.py
+++ tests/test_pivot.py
@@ -3,24 +3,22 @@ import unittest
 
 from context import grama as gr
 from context import data
-from pandas import DataFrame
+from pandas import DataFrame, RangeIndex
 from pandas.testing import assert_frame_equal
-
-
-# python -m unittest test_pivot.TestPivotWider.test_pivot_wider && python -m unittest test_pivot.TestPivotLonger.test_pivot_longer
 
 
 class TestPivotLonger(unittest.TestCase):
     """Test implementation of pivot_longer
     """
     def test_pivot_longer(self):
-        data = {
-            "One": {"A": 1.0, "B": 2.0, "C": 3.0},
-            "Two": {"A": 1.0, "B": 2.0, "C": 3.0},
-        }
-
-        wide = DataFrame(data)
-        long = gr.tran_pivot_longer(wide, columns=("One","Two"), index_to="index", names_to="columns", values_to="values")
+        wide = DataFrame(
+            {
+                "One": {"A": 1.0, "B": 2.0, "C": 3.0},
+                "Two": {"A": 1.0, "B": 2.0, "C": 3.0},
+            }
+        )
+        long = gr.tran_pivot_longer(wide, columns=("One","Two"),
+        index_to="index", names_to="columns", values_to="values")
 
         expected = DataFrame(
             {
@@ -30,33 +28,71 @@ class TestPivotLonger(unittest.TestCase):
             }
         )
 
-        # print(wide)
-        # print(long)
-        # print(expected)
         assert_frame_equal(long, expected)
 
-    def test_pivot_longer_bigDF(self):
-        trajectory_windowed = data.df_trajectory_windowed
-        long = DataFrame(trajectory_windowed)
-        print(long)
-        # wide = gr.tran_pivot_wider(long,indexes="t",names_from = "x",values_from="y")
-        # print(wide)
-        # to note all this data is very tidy and hard to pivot_wider
-        # since it possess multi dependecny or lack of index column
+
+    def test_pivot_longer_preservation(self):
+        """ Test pivot_longer preserves unsued rows if index_to is None
+        """
+        stang = data.df_stang_wide
+        long = gr.tran_pivot_longer(
+            stang,
+            index_to = None,
+            columns=["E_00","mu_00","E_45","mu_45","E_90","mu_90"],
+            names_to="var",values_to="val"
+        )
+
+        expected = gr.tran_pivot_longer(
+            stang,
+            index_to = ["thick","alloy"],
+            columns=["E_00","mu_00","E_45","mu_45","E_90","mu_90"],
+            names_to="var",values_to="val"
+        )
+
+        assert_frame_equal(long, expected)
+
+
+    def test_pivot_longer_preservation_index_has_no_label(self):
+        """ Test if pivot_longer preserves unused index if no name
+            provided for it
+        """
+        wide = DataFrame(
+            {
+                "One": {"A": 1.0, "B": 2.0, "C": 3.0},
+                "Two": {"A": 1.0, "B": 2.0, "C": 3.0},
+            }
+        )
+        long = gr.tran_pivot_longer(wide, columns=("One","Two"),
+        index_to=None, names_to="columns", values_to="values")
+
+        expected = DataFrame(
+            {
+                "index": ["A", "B", "C", "A", "B", "C"],
+                "columns": ["One", "One", "One", "Two", "Two", "Two"],
+                "values": [1.0, 2.0, 3.0, 1.0, 2.0, 3.0],
+            }
+        )
+
+        assert_frame_equal(long, expected)
 
 
 class TestPivotWider(unittest.TestCase):
     """Test implementation of pivot_wider
     """
     def test_pivot_wider(self):
-        data = {
-            "index": ["A", "B", "C", "C", "B", "A"],
-            "columns": ["One", "One", "One", "Two", "Two", "Two"],
-            "values": [1.0, 2.0, 3.0, 3.0, 2.0, 1.0],
-        }
-
-        long = DataFrame(data)
-        wide = gr.tran_pivot_wider(long,indexes="index",names_from = "columns",values_from="values")
+        long = DataFrame(
+            {
+                "loo": ["A", "B", "C", "A", "B", "C"],
+                "columns": ["One", "One", "One", "Two", "Two", "Two"],
+                "values": [1.0, 2.0, 3.0, 1.0, 2.0, 3.0],
+            }
+        )
+        wide = gr.tran_pivot_wider(
+            long,
+            indexes_from="loo",
+            names_from="columns",
+            values_from="values"
+        )
 
         expected = DataFrame(
             {
@@ -64,8 +100,34 @@ class TestPivotWider(unittest.TestCase):
                 "Two": {"A": 1.0, "B": 2.0, "C": 3.0},
             }
         )
+        expected.insert(0,"loo",["A","B","C"])
+        expected.index=RangeIndex(start=0,stop=len(expected),step=1)
 
-        # print(long)
-        # print(wide)
-        # print(expected)
+        assert_frame_equal(wide, expected)
+
+
+    def test_pivot_wider_preservation(self):
+        """ Test if pivot_wider preserves indexed columns pivoted around when
+            indexes_from is None
+        """
+        stang = data.df_stang_wide
+        long = gr.tran_pivot_longer(
+            stang,
+            index_to = ["thick","alloy"],
+            columns=["E_00","mu_00","E_45","mu_45","E_90","mu_90"],
+            names_to="var",values_to="val"
+        )
+        wide = gr.tran_pivot_wider(
+            long,
+            indexes_from=None,
+            names_from="var",
+            values_from="val"
+        )
+        expected = gr.tran_pivot_wider(
+            long,
+            indexes_from=["thick","alloy"],
+            names_from="var",
+            values_from="val"
+        )
+
         assert_frame_equal(wide, expected)
