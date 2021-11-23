6657db9fdcae5db4bd210bab2b02cae2260a531a
Zach <zdelrosario@outlook.com>
2021-11-23 13:54:24 -0500

    add tests for eval_contour

---
+++ tests/test_contour.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pandas as pd
+import unittest
+
+from context import grama as gr
+
+##################################################
+class TestContour(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_contour(self):
+        md1 = (
+            gr.Model()
+            >> gr.cp_vec_function(
+                fun=lambda df: gr.df_make(
+                    f=df.x**2 + df.y**2,
+                    g=df.x + df.y,
+                ),
+                var=["x", "y"],
+                out=["f", "g"],
+            )
+            >> gr.cp_bounds(
+                x=(-1, +1),
+                y=(-1, +1),
+            )
+        )
+
+        md2 = (
+            gr.Model()
+            >> gr.cp_vec_function(
+                fun=lambda df: gr.df_make(
+                    f=(df.c) * df.x + (1 - df.c) * df.y,
+                ),
+                var=["x", "y", "c"],
+                out=["f"],
+            )
+            >> gr.cp_bounds(
+                x=(-1, +1),
+                y=(-1, +1),
+                c=(+0, +1),
+            )
+        )
+
+        ## Basic functionality
+        df_res1 = (
+            md1
+            >> gr.ev_contour(
+                var=["x", "y"],
+                out=["f", "g"],
+                n_side=10, # Coarse, for speed
+            )
+        )
+        # Contains correct columns
+        self.assertTrue("x" in df_res1.columns)
+        self.assertTrue("y" in df_res1.columns)
+
+        df_res2 = (
+            md2
+            >> gr.ev_contour(
+                var=["x", "y"],
+                out=["f"],
+                df=gr.df_make(c=[0, 1]),
+                n_side=10, # Coarse, for speed
+            )
+        )
+        # Contains auxiliary variable
+        self.assertTrue("c" in df_res2.columns)
+
+        ## Check assertions
+        # No var
+        with self.assertRaises(ValueError):
+            (
+                md1
+                >> gr.ev_contour(out=["f"])
+            )
+
+        # Incorrect number of inputs
+        with self.assertRaises(ValueError):
+            (
+                md1
+                >> gr.ev_contour(var=["x", "y", "z"])
+            )
+
+        # Unavailable inputs
+        with self.assertRaises(ValueError):
+            (
+                md1
+                >> gr.ev_contour(var=["foo", "bar"])
+            )
+
+        # Unsupported input
+        with self.assertRaises(ValueError):
+            (
+                md2
+                >> gr.ev_contour(
+                    var=["x", "y"],
+                    out=["f"],
+                )
+            )
+
+        with self.assertRaises(ValueError):
+            (
+                md2
+                >> gr.ev_contour(
+                    var=["x", "y"],
+                    out=["f"],
+                    df=gr.df_make(foo=1)
+                )
+            )
+
+        # Zero bound width
+        with self.assertRaises(ValueError):
+           (
+               gr.Model()
+               >> gr.cp_vec_function(
+                   fun=lambda df: gr.df_make(
+                       f=df.x**2 + df.y**2,
+                       g=df.x + df.y,
+                   ),
+                   var=["x", "y"],
+                   out=["f", "g"],
+               )
+               >> gr.cp_bounds(
+                   x=( 0,  0),
+                   y=(-1, +1),
+               )
+
+               >> gr.ev_contour(
+                   var=["x", "y"],
+                   out=["f", "g"],
+                   n_side=10, # Coarse, for speed
+               )
+           )
+
+        # No out
+        with self.assertRaises(ValueError):
+            (
+                md1
+                >> gr.ev_contour(var=["x", "y"])
+            )
+
+        # Output unavailable
+        with self.assertRaises(ValueError):
+            (
+                md1
+                >> gr.ev_contour(var=["x", "y"], out=["foo"])
+            )
