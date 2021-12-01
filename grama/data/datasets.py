ea1f4a02a9dc84f695b2cfff1d81e91926c77565
zdelrosario <zdelrosario@outlook.com>
2021-11-30 21:32:11 -0500

    add channel DNS data

--- grama/data/datasets.py
+++ grama/data/datasets.py
@@ -1,4 +1,5 @@
 __all__ = [
+    "df_channel",
     "df_diamonds",
     "df_ruff",
     "df_stang",
@@ -34,3 +35,38 @@ df_trajectory_windowed = read_csv(Path(path_grama / "data/trajectory_windowed.cs
 
 # Shewhart
 df_shewhart = read_csv(Path(path_grama / "data/shewhart1931-table3.csv"))
+
+# PSAAP II IJMF (2020) data
+df_channel = read_csv(Path(path_grama / "data/psaap.csv"))
+df_channel.rename(
+    columns=dict(
+        # xi="x",
+        x="xi",
+        # H="W",
+        W="H",
+        # U="U_0",
+        U_0="U",
+        # T_0="T_f",
+        T_f="T_0",
+        # cp_p="C_pv",
+        C_pv="cp_p",
+        # cp_f="C_fp",
+        C_fp="cp_f",
+        # nu_f="mu_f",
+        mu_f="nu_f",
+        # Q_abs="eps_p",
+        eps_p="Q_abs",
+        # h_p="h",
+        h="h_p",
+    ),
+    inplace=True,
+)
+# Compute thermal diffusivity from initial temperature
+df_channel["alpha_f"] = (
+      1.862e1
+    + 1.327e-1 * df_channel.T_0
+    + 1.026e-4 * df_channel.T_0**2
+    - 5.270e-9 * df_channel.T_0**3
+)
+# Back-calculate the proper number density
+df_channel["n"] = df_channel.N_p / df_channel.H**2 / df_channel.L
