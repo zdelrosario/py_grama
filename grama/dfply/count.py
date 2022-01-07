95e8677e91ee7e85b2c8a73e04a9885e5d233958
zdelrosario <zdelrosario@outlook.com>
2022-01-07 16:18:03 -0500

    add docstring

--- grama/dfply/count.py
+++ grama/dfply/count.py
@@ -10,6 +10,32 @@ from pandas import DataFrame
 @dfdelegate
 def tran_count(df, *args):
     r"""Count occurrences according to groups
+
+    Count the number of rows within a specified grouping.
+
+    Arguments
+        df (DataFrame): Data to compute counts over.
+        *args (str or gr.Intention()): Columns for grouping.
+
+    Returns:
+        DataFrame: Result of group counting.
+
+    Examples:
+        >>> import grama as gr
+        >>> from grama.data import df_diamonds
+        >>> DF = gr.Intention()
+        >>> (
+        >>>     # Single group variable
+        >>>     df_diamonds
+        >>>     >> gr.tf_count(DF.cut)
+        >>> )
+        >>>
+        >>> (
+        >>>     # Multiple group variables
+        >>>     df_diamonds
+        >>>     >> gr.tf_count(DF.cut, DF.clarity)
+        >>> )
+
     """
     # Compute the count
     df_res = DataFrame(
