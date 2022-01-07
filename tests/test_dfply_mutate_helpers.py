39c086a7e87df0a09c45e9f0b935ded2647bcde6
zdelrosario <zdelrosario@outlook.com>
2022-01-07 08:55:38 -0500

    add unittest for qqvals

--- tests/test_dfply_mutate_helpers.py
+++ tests/test_dfply_mutate_helpers.py
@@ -4,7 +4,9 @@ from context import grama as gr
 from context import data
 
 from pandas import Series
-from numpy import NaN
+from numpy import NaN, arange
+from numpy.random import shuffle
+from scipy.stats import norm
 
 X = gr.Intention()
 
@@ -22,12 +24,20 @@ class TestFactors(unittest.TestCase):
 
         self.assertTrue(list(ang_fct.categories) == [0, 90, 45])
 
+class TestPandasUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
     def test_fillna(self):
         s_nan = Series([NaN] * 2)
         s_filled = gr.fillna(s_nan, value=0)
         s_true = Series([0.0] * 2)
 
         self.assertTrue((s_filled == s_true).all())
+
+class TestPareto(unittest.TestCase):
+    def setUp(self):
+        pass
 
     def test_pareto_min(self):
         df_test = gr.df_make(
@@ -68,3 +78,25 @@ class TestFactors(unittest.TestCase):
         # Check for ValueError
         with self.assertRaises(ValueError):
             gr.stratum_min([1], [1, 2, 3])
+
+class TestQQ(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_qqvals(self):
+        # Set up data
+        n = 10
+        i = arange(1, n + 1)
+        p = (i - 0.3175) / (len(i) + 0.365)
+        p[0] = 0.5**n
+        p[-1] = 1 - 0.5**n
+        q = norm.ppf(p)
+
+        # Correct values
+        marg = gr.marg_mom("norm", mean=0, sd=1) # Use true distribution
+        q_res = gr.qqvals(q, marg=marg)
+        self.assertTrue((q == q_res).all())
+
+        # Handles shuffling
+        shuffle(q)
+        self.assertTrue((q == gr.qqvals(q, marg=marg)).all())
