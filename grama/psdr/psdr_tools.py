73b2e8fe77b40c74488a65cca06484d82e6ef83c
zdelrosario <zdelrosario@outlook.com>
2022-03-02 14:27:42 -0500

    refactor polyridge code into common low-level interface

--- grama/psdr/psdr_tools.py
+++ grama/psdr/psdr_tools.py
@@ -52,16 +52,17 @@ class FunctionPoly(Function):
         y = self.regressor.predict(df[self.var])
         return DataFrame(data=y, columns=self.out)
 
-## Interfaces
+
+## Implementation
 # --------------------------------------------------
-@curry
-def tran_polyridge(
+def _polyridge(
     df, var=None, out=None, n_dim=None, n_degree=None, **kwargs
 ):
-    r"""Polynomial Ridge Approximation for parameter space reduction
+    r"""Low-level interface for polyridge
 
     Apply the polynomial ridge approximation to seek a low-dimensional subspace
-    among inputs that best explain variability in a selected output.
+    among inputs that best explain variability in a selected output. Return the
+    scikit-style fitted object and modified `var`.
 
     Args:
         df (DataFrame): Target dataset to reduce
@@ -75,41 +76,14 @@ def tran_polyridge(
         n_init (int): Number of iterations in optimization
 
     Returns:
-        DataFrame: Set of weights defining the dimension-reduced space.
+        PolynomialRidgeApproximation: Scikit-style regressor
+        list of str: Modified variable list
 
     Notes:
-        - A wrapper for psdr.PolynomialRidgeApproximation
+        - A wrapper for PolynomialRidgeApproximation, originally implemented in the psdr package.
 
     References:
         - J. M. Hokanson and Paul G. Constantine. Data-driven Polynomial Ridge Approximation Using Variable Projection. SIAM J. Sci. Comput. Vol 40, No 3, pp A1566–A1589, DOI:10.1137/17M1117690.
-
-    Examples:
-        >>> import grama as gr
-        >>> from plotnine import *
-        >>> from grama.psdr import tf_polyridge
-        >>> DF = gr.Intention()
-        >>>
-        >>> ## Set up a dataset to reduce
-        >>> df_data = (
-        >>>     gr.df_make(x=range(10))
-        >>>     >> gr.tf_outer(gr.df_make(y=range(10)))
-        >>>     >> gr.tf_outer(gr.df_make(z=range(10)))
-        >>>     >> gr.tf_mutate(f=DF.x - DF.y)
-        >>> )
-        >>>
-        >>> ## Use polynomial ridge approximation to derive weights
-        >>> df_weights = (
-        >>>     df_data
-        >>>     >> tf_polyridge(out="f", n_dim=1, n_degree=1)
-        >>> )
-        >>>
-        >>> ## Construct a shadow plot
-        >>> (
-        >>>     df_data
-        >>>     >> gr.tf_inner(df_weights)
-        >>>     >> ggplot(aes("dot", "f"))
-        >>>     + geom_point()
-        >>> )
 
     """
     ## Check invariants
@@ -142,6 +116,70 @@ def tran_polyridge(
         **kwargs,
     )
     pr.fit(df[var].values, df[out].values)
+
+    return pr, var
+
+## Interfaces
+# --------------------------------------------------
+@curry
+def tran_polyridge(
+    df, var=None, out=None, n_dim=None, n_degree=None, **kwargs
+):
+    r"""Polynomial Ridge Approximation for parameter space reduction
+
+    Apply the polynomial ridge approximation to seek a low-dimensional subspace
+    among inputs that best explain variability in a selected output.
+
+    Args:
+        df (DataFrame): Target dataset to reduce
+        var (list or None): Variables in df on which to perform dimension reduction.
+            Use None to compute with all numeric variables, except `out`.
+        out (string): Name of target output
+        n_dim (int): Target dimensionality
+        n_degree (int): Fitted polynomial (total) order
+
+    Kwargs:
+        n_init (int): Number of iterations in optimization
+
+    Returns:
+        DataFrame: Set of weights defining the dimension-reduced space.
+
+    Notes:
+        - A wrapper for PolynomialRidgeApproximation, originally implemented in the psdr package.
+
+    References:
+        - J. M. Hokanson and Paul G. Constantine. Data-driven Polynomial Ridge Approximation Using Variable Projection. SIAM J. Sci. Comput. Vol 40, No 3, pp A1566–A1589, DOI:10.1137/17M1117690.
+
+    Examples:
+        >>> import grama as gr
+        >>> from grama.psdr import tf_polyridge
+        >>> DF = gr.Intention()
+        >>>
+        >>> ## Set up a dataset to reduce
+        >>> df_data = (
+        >>>     gr.df_make(x=range(10))
+        >>>     >> gr.tf_outer(gr.df_make(y=range(10)))
+        >>>     >> gr.tf_outer(gr.df_make(z=range(10)))
+        >>>     >> gr.tf_mutate(f=DF.x - DF.y)
+        >>> )
+        >>>
+        >>> ## Use polynomial ridge approximation to derive weights
+        >>> df_weights = (
+        >>>     df_data
+        >>>     >> tf_polyridge(out="f", n_dim=1, n_degree=1)
+        >>> )
+        >>>
+        >>> ## Construct a shadow plot
+        >>> (
+        >>>     df_data
+        >>>     >> gr.tf_inner(df_weights)
+        >>>     >> gr.ggplot(gr.aes("dot", "f"))
+        >>>     + gr.geom_point()
+        >>> )
+
+    """
+    ## Run the low-level implementation
+    pr, var = _polyridge(df, var=var, out=out, n_dim=n_dim, n_degree=n_degree, **kwargs)
 
     ## Package the results
     df_res = DataFrame(
@@ -211,36 +249,8 @@ def fit_polyridge(
             )
 
     """
-    ## Check invariants
-    if out is None:
-        raise ValueError("Must select an `out` column.")
-    if not (out in df.columns):
-        raise ValueError("out={} is not a column in provided dataframe.".format(out))
-
-    if var is None:
-        var = set(df.select_dtypes(include=[npnumber]).columns.values)
-        var = list(var.difference({out}))
-    else:
-        var = list(var).copy()
-        diff = set(var).difference(set(df.columns))
-        if len(diff) > 0:
-            raise ValueError(
-                "`var` must be subset of `df.columns`\n" "diff = {}".format(diff)
-            )
-
-    if (n_degree == 1) and (n_dim > 1):
-        raise ValueError(
-            "n_dim > 1 cannot work with n_degree == 1; this would be " +
-            "an over-parameterized linear model. Try again with n_degree > 1."
-        )
-
-    ## Compute subspace reduction
-    pr = PolynomialRidgeApproximation(
-        n_degree,
-        n_dim,
-        **kwargs,
-    )
-    pr.fit(df[var].values, df[out].values)
+    ## Run the low-level implementation
+    pr, var = _polyridge(df, var=var, out=out, n_dim=n_dim, n_degree=n_degree, **kwargs)
 
     ## Package the results
     md_res = Model(
