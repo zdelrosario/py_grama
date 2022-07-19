a3f1b797eaae143dc643213907145d3bddf4616b
mstites <maeve@mstites.com>
2022-07-19 16:02:53 -0400

    add arg names

--- grama/eval_pnd.py
+++ grama/eval_pnd.py
@@ -107,8 +107,8 @@ def eval_pnd(model, df_train, df_test, signs, n=int(1e4), seed=None, append=True
     # if not isinstance(df_test, DataFrame):
     #     raise TypeError('df_test must be a DataFrame')
     invariants_eval_model(model)
-    invariants_eval_df(df_train)
-    invariants_eval_df(df_test)
+    invariants_eval_df(df_train, arg_name="df_train")
+    invariants_eval_df(df_test, arg_name="df_test")
 
 
     # Check content
