e8a63aac3be03920f8263ea03df00d215762615f
Zach <zdelrosario@outlook.com>
2023-12-14 11:59:16 -0500

    version 0.3.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="py_grama",
     author="Zachary del Rosario",
     author_email="zdelrosario@outlook.com",
-    version="0.3.6",
+    version="0.3.7",
     packages=[
         "grama",
         "grama.data",
