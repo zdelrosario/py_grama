0180c566bf240b84d1f49fc8368f7446b76e4d94
zdelrosario <zdelrosario@outlook.com>
2024-08-01 08:57:53 -0400

    cover find_files with tests

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,10 +1,7 @@
-import numpy as np
-import pandas as pd
-from scipy.stats import norm
 import unittest
-
+from os.path import join
 from context import grama as gr
-from context import models, data
+from context import models
 
 ## Core function tests
 ##################################################
@@ -15,3 +12,22 @@ class TestPipe(unittest.TestCase):
     def test_pipe(self):
         ## Chain
         res = self.md >> gr.ev_hybrid(df_det="nom") >> gr.tf_sobol()
+
+class TestFindFiles(unittest.TestCase):
+    def test_find_files(self):
+        # Non-recursive
+        res = gr.find_files(".", ".py", recursive=False)
+        self.assertTrue(
+            join(".", "context.py") in res
+        )
+        self.assertTrue(
+            not join("longrun", "sp_convergence.ipynb") in res
+        )
+        # No return
+        res = gr.find_files(".", ".exe", recursive=False)
+        self.assertTrue(len(res) == 0)
+        # Recursive
+        res = gr.find_files(".", ".ipynb", recursive=True)
+        self.assertTrue(
+            join(".", "longrun", "sp_convergence.ipynb") in res
+        )
